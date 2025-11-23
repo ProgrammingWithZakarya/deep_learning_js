fd6c6004c8a218bfd00e0e312eb2df2eb4e28a39
zakarya <hasanzadahzakarya@gmail.com>
2025-11-23 16:32:25 +0300

    variables types - data types - explicit and implicit convertions - oprators

---
+++ js/ep01/script.js
@@ -0,0 +1,55 @@
+// var, let, const
+// var => hoisting
+// let => scope
+// const: constant => scope
+
+
+// data types
+// 1. Premitive Type
+// 2. Refrence Type (not premitive)
+
+// 1.
+// 1.1 string
+// 1.2 number
+// 1.3 boolean
+// 1.4 null 
+// 1.5 undefined
+// 1.6 symbol
+// 1.7 bigint
+
+
+// 2.
+// 2.1 Object 
+// 2.2 Function
+// 2.3 Array
+
+// تبدیل صریح
+// String(), Number(), Boolean()
+
+
+// تبدیل ضمنی
+const a = 1 + '3' //'13'
+const b = '3' + 1 // '31'
+const c = 1 + 2 + '3' // '33'
+const d = '3' - 1 // 2
+const g = '9' / 3 // 3
+
+
+// falsy values: false, null, undefiend, '', 0, -0, NaN, 0n;
+
+
+// operators 
+
+// ریاضی
+// + - / * % ** 
+// += -= /= *= %= **=
+
+// مقایسه
+// < > <= >=
+// == === != !==
+
+// منطقی
+// ! || &&
+
+// باینری
+// | &
